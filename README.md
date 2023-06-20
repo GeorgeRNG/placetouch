83e03aab877d6b97b542ffbc69df2dfb75b814c6
GeorgeRNG <grogrng@gmail.com>
2023-06-20 19:52:03 +0100

    lots of stuff

---
+++ README.md
@@ -0,0 +1,17 @@
+# placetouch
+Python script for wayland to make your touchpad static.  
+This means, whereever you touch on your touchpad, your mouse will go.  
+Touch the bottom right, your mouse will go to the bottom right. The middle? ditto.
+
+## Install
+This was made and tested on ubuntu desktop 23.04  
+Install all the dependencies:  
+`sudo apt install git python3 python3-evdev python3-uinput python3-tk`  
+Then clone the repository:  
+`git clone https://github.com/GeorgeRNG/placetouch`  
+
+## Use
+Open the folder in a terminal, and run  
+`sudo python3 main.py`  
+Move your finger to the far ends of your touchpad to calibrate   
+You might want to disable the default touchpad in settings, the function key may or might not break this.  
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-
-from evdev import InputDevice
+import evdev
 import time
 import uinput
-#SET THIS TO YOUR DEVICE
-touchPad = InputDevice('/dev/input/event6')
+import threading
+import tkinter as tk
+
+touchPad = 0
+
+options = list(map(lambda n: evdev.InputDevice(n), evdev.list_devices()))
+optionNames = list(map(lambda n: n.path + ": " + n.name, options))
+for (i, device) in enumerate(options):
+    if("touchpad" in device.name.lower()):
+        touchPad = i
+
 
 touchHeight = 0
 touchWidth = 0
@@ -11,13 +19,44 @@ touchWidth = 0
 screenHeight = 530
 screenWidth = 970
 
-doTouchWidthStart = 500
-doTouchHeightStart = 500
+leftBorder = 200
+rightBorder = 400
+topBorder = 100
+bottomBorder = 100
 
-doTouchWidthEnd = 500
-doTouchHeightEnd = 500
+fingerI = 0
+x = 0
+y = 0
+clicking = False
 
-def main():
+touchPadThread = None
+
+window = tk.Tk()
+xText = tk.StringVar()
+xText.set("X: -")
+yText = tk.StringVar()
+yText.set("Y: -")
+clickingText = tk.StringVar()
+clickingText.set("Clicking: False")
+selectedEvent = tk.StringVar()
+selectedEvent.set(optionNames[touchPad])
+def mainWindow():
+    title = tk.Label(text="placetouch")
+    title.pack()
+    touchpadSelection = tk.OptionMenu(window,selectedEvent,*optionNames)
+    touchpadSelection.pack()
+    xLabel = tk.Label(textvariable=xText)
+    xLabel.pack()
+    yLabel = tk.Label(textvariable=yText)
+    yLabel.pack()
+    clickingLable = tk.Label(textvariable=clickingText)
+    clickingLable.pack()
+    exitButton = tk.Button(text="Exit",command=exit)
+    exitButton.pack()
+    window.mainloop()
+
+def touchpad():
+
     events = (
         uinput.REL_X,
         uinput.REL_Y,
@@ -29,27 +68,51 @@ def main():
 
         time.sleep(1)
 
-        for event in touchPad.read_loop():
+        for event in options[touchPad].read_loop():
             get_xy_coords(event,device)
 
 def get_xy_coords(e,device):
-    if e.code == 53:
-        global touchWidth
-        if(e.value > touchWidth):
-            touchWidth = e.value
+    global fingerI
+    if e.code == 47: # finger index
+        fingerI = e.value
 
-        pointInTouchPad = e.value / touchWidth
+    if e.code == 57: # some touch index thing
+        if(e.value == -1):
+            fingerI = 0
 
-        device.emit(uinput.REL_X, -10000)
-        device.emit(uinput.REL_X, int(pointInTouchPad * screenWidth))
-    if e.code == 54:
-        global touchHeight
-        if(e.value > touchHeight):
-            touchHeight = e.value
+    if fingerI == 0:
+        if e.code == 53:
+            global touchWidth
+            global x
+            x = e.value
+            if(e.value > touchWidth):
+                touchWidth = e.value
+            xText.set("Touch X: " + str(x) + " / " + str(touchWidth))
 
-        device.emit(uinput.REL_Y, -10000)
-        device.emit(uinput.REL_Y, int((e.value / touchHeight) * screenHeight))
+            pointInTouchPad = (e.value - leftBorder) / (touchWidth - (leftBorder + rightBorder))
 
+            device.emit(uinput.REL_X, -10000)
+            device.emit(uinput.REL_X, int(pointInTouchPad * screenWidth))
+        if e.code == 54:
+            global touchHeight
+            global y
+            y = e.value
+            if(e.value > touchHeight):
+                touchHeight = e.value
+            yText.set("Touch Y: " + str(y) + " / " + str(touchHeight))
+
+            pointInTouchPad = (e.value - topBorder) / (touchHeight - (topBorder + bottomBorder))
+
+            device.emit(uinput.REL_Y, -10000)
+            device.emit(uinput.REL_Y, int(pointInTouchPad * screenHeight))
+
+    if e.code == 272:
+        clicking = e.value == 1
+        clickingText.set("Clicking: " + str(clicking))
+        device.emit(uinput.BTN_LEFT, e.value)
 
 if __name__ == "__main__":
-    main()
+    touchPadThread = threading.Thread(target=touchpad)
+    touchPadThread.daemon = True
+    touchPadThread.start()
+    mainWindow()
